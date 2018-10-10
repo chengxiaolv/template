b40049b1ad03d31ddcf01adbc97440eb759d0e51
chengxiaolv <972987164@qq.com>
2018-10-10 17:02:09 +0800

    第一次提交

---
+++ src/components/homePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+<ul>
+  <li>s</li>
+  <li>s</li>
+  <li>s</li>
+</ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
